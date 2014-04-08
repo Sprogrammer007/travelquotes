be7f7a5bf631270b32eda9886d72e2e4446f317c
Sprogrammer007 <stevenag006@hotmail.com>
2014-04-08 18:45:44 -0400

    some updates

--- app/admin/policy.rb
+++ app/admin/policy.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Policy do
   config.sort_order = "id_asc"
   menu :parent => "Companies"
-  filter :age_bracket, :collection => proc {(A.all).map{|c| [c.policy_name, c.id]}}
+  filter :age_bracket, :collection => proc {(AgeBracket.all).map{|c| [c.policy_name, c.id]}}
 
   include_import
 end
