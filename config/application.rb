8c763915bd810d63a7a010b87c03a64f28228334
Steve <stevenag006@hotmail.com>
2014-06-08 18:08:53 -0400

    tiny mce

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module Travelquotes
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(tiny_mce/*)
-    config.assets.precompile += %w( *.css *.js )
   end
 end
