05a48bb0a5a07e4827aa23401a07ff3bbce4012b
Steve <stevenag006@hotmail.com>
2014-07-30 14:26:45 -0400

    legal text by filters

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -305,7 +305,7 @@ ActiveAdmin.register Product do
 			end
 			f.input :name, label: "Policy Name"
 			f.input :policy_number
-			f.input :policy_type, :as => :select, :collection => options_for_select(["Visitor Medical Emergency", "All Inclusive" "Student Medical Inbound", "Canadian Ex-Pat"], (params[:p_type] || "Visitor Medical Emergency"))
+			f.input :policy_type, :as => :select, :collection => options_for_select(["Visitor Medical Emergency", "All Inclusive", "Student Medical Inbound", "Canadian Ex-Pat"], (params[:p_type] || "Visitor Medical Emergency"))
 			f.input :description, input_html: {class: "tinymce"}
 			f.input :min_price
 			f.input :can_buy_after_30_days, :as => :radio, :collection => [["Yes", true], ["No", false]]
