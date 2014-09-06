cb519f00abd0c036e93cf1c972d27e3d1ce474cd
Steve <stevenag006@hotmail.com>
2014-09-06 10:20:24 -0400

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,7 @@ Travelquotes::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'travelquotestest.heroku.com' }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
