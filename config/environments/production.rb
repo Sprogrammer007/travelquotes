43ace27b89e044f05ada3810c7fa4f71065e1245
Steve <stevenag006@hotmail.com>
2014-09-06 10:12:20 -0400

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,18 @@ Travelquotes::Application.configure do
     }
   }
 
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "stone-hedgefinancialgroup.ca",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['MAIL_USER'],
+    password: ENV['MAIL_PASS']
+  }
+
 end
